3a66b338bee0445d50ac1023baec4fd4d7eaf071
PrinceRaval <divya.raval92@gmail.com>
2024-04-25 11:17:43 -0500

    new chages had a typo in javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const intialCards = [
+const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -25,4 +25,4 @@ const intialCards = [
   },
 ];
 
-console.log(intialCards);
+console.log(initialCards);
