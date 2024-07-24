22ef89df7409ea3ebbb03c47a9b05d7c778fe2d6
PrinceRaval <divya.raval92@gmail.com>
2024-07-24 14:35:54 -0500

    new changes

--- README.md
+++ README.md
@@ -2,21 +2,44 @@
 
 ### Overview
 
-Project features
--Semantic HTML5
--Flexbox/Grid layout
--Responsive design via media queries
--Flat BEM structure
--Card elements and images rendered through Javascript using element
--Interactive modal boxes for editing profile information, adding new card elements, and viewing a "preview" popup of card image when clicked on
+- Introduction to JavaScript
+- Adaptive Web Design
+- TripleTen Sprint 5
 
--Photos
--Video
--Interactive Website
+**Intro to Javascript and Adaptive Web Design**
 
-"This project showcases various parts of the US along with their tourist attractions. I had the opportunity to develop a comprehensive website optimized for mobile, tablet, and desktop devices. Although the project posed challenges, it was also a rewarding and entertaining experience for me. Through overcoming these challenges, I've gained valuable skills and can now confidently build responsive websites for all three platforms."
+This project is made so all the elements are displayed correctly on popular screen sizes. JavaSript was used to duplicate the cards displayed instead of having multiple cards in the HTML. This is my first project using Javascript and I will continue to improve on it as my skills in the language advance.
 
-video of the project overview
-https://www.loom.com/share/a219a404b6b642b1a1637f5d24b4d4a0?sid=e4293e06-5bf0-4f21-bf37-7ae0daa73ac6
-https://prince21241.github.io/se_project_aroundtheus/
-https://ibb.co/XbZRztR
+**Figma**
+
+- [Link to the project on Figma](https://www.figma.com/file/ii4xxsJ0ghevUOcssTlHZv/Sprint-3%3A-Around-the-US?node-id=0%3A1)
+
+**TripleTen Sprint 5**
+
+This is the 5th Sprint in the coding bootcamp TripleTen. Working with them had been such a pleasure so far. The entire TripleTen team has been a huge help with any questions or problems I had with the project and I can't wait to continue to work with them to develop my skills as a Software Engineer.
+
+## Update 1: Added Animation to the site
+
+Added animation when you hover over profile buttons. Added heart throb to like buttons.
+
+![Around The US Gif](https://github.com/ErickTgit/se_project_aroundtheus/assets/145604820/eae91fea-a5a2-49ef-b158-34d83ff07441)
+
+> [!NOTE]
+> The heart animation has since been removed.
+
+## Update 2: New, Cleaner JavaScript code
+
+Removed previous animation and added new features such as :
+
+- Change profile name
+- Change profile Description
+- Add new cards to page
+- Preview images by clicking them
+- Transitions for opening each Modal
+- Ability to delete cards
+
+![Sprint 5 Preview 3](https://github.com/ErickTgit/se_project_aroundtheus/assets/145604820/4099f1d5-fe11-4cd9-b1b7-04f0bdc65156)
+
+## Goals for Next Update
+
+So far, the frontend of this page is nearly complete. Features I would like to add in the future are a way to change the profile picture, as well as fix a bug where if an image is too large it displays outside of the screen. This can be seen when you preview Yosemite Valley for example.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,128 +1,239 @@
-/* -------------------------------------------------------------------------- */
-/*                                   Import                                   */
-/* -------------------------------------------------------------------------- */
-import "./index.css";
-import { initialCards, config } from "../utils/constants.js";
+import Api from "../components/Api.js";
+import { config } from "../utils/constants.js";
+import { initialCards } from "../utils/constants.js";
+import UserInfo from "../components/UserInfo.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import Section from "../components/Section.js";
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
-import Section from "../components/Section.js";
-import PopupWithImage from "../components/PopupWithImage.js";
-import PopupWithForm from "../components/PopupWithForm.js";
-import UserInfo from "../components/UserInfo.js";
+import "../pages/index.css";
+import PopupDeleteCard from "../components/PopupDeleteCard.js";
 
-/* -------------------------------------------------------------------------- */
-/*                                 used const                                 */
-/* -------------------------------------------------------------------------- */
-const profileEditButton = document.querySelector("#profile-edit-button");
-const profileEditForm = document.querySelector("#profile-form");
-const cardAddButton = document.querySelector("#card-add-button");
-const cardAddForm = document.querySelector("#add-card-form");
-const profileTitleInput = document.querySelector("#profile-title-input");
-const profileDescriptionInput = document.querySelector(
-  "#profile-description-input"
+const profileImageEditButton = document.getElementById("avatar-button");
+const profileEditButton = document.getElementById("profile-edit-button");
+const addNewCardButton = document.getElementById("profile-add-button");
+const profileTitleInput = document.getElementById("profile-title-input");
+const profileDescriptionInput = document.getElementById(
+  "profile-description-input"
 );
 
-/* -------------------------------------------------------------------------- */
-/*                                     OOP                                    */
-/* -------------------------------------------------------------------------- */
-/* --------------------------- FormValidator(Form) -------------------------- */
+const popupWithImage = new PopupWithImage("#image-modal");
+popupWithImage.setEventListeners();
+
+const deleteImageModal = new PopupDeleteCard("#delete-card-modal");
+deleteImageModal.setEventListeners();
+
+const profileEditModal = new PopupWithForm(
+  "#profile-edit-modal",
+  (formData) => {
+    handleEditProfileSubmit(formData);
+  }
+);
+
+profileEditModal.setEventListeners();
+
+//Declare API class:
+
+const api = new Api({
+  baseUrl: "https://around-api.en.tripleten-services.com/v1",
+  headers: {
+    authorization: "2db58c83-3523-4135-8564-2660432887a4",
+    "Content-Type": "application/json",
+  },
+});
+
+//Declare UserInfo Class:
+const userInfo = new UserInfo({
+  titleSelector: ".profile__title",
+  descriptionSelector: ".profile__description",
+  profileImage: ".profile__image",
+});
+
+//Declare PopupWithForm for addCardModal:
+const addCardPopup = new PopupWithForm("#add-card-modal", (formData) => {
+  handleAddCardSubmit(formData);
+});
+addCardPopup.setEventListeners();
+
+//declare Edit Image Modal
+const profileImageModal = new PopupWithForm(
+  "#profile-image-modal",
+  (formData) => {
+    handleChangeProfilePicture(formData.url);
+  }
+);
+profileImageModal.setEventListeners();
+
+//Declare Section for Initial Cards
+let cardSection;
+
+//RenderCardFunction
+function renderCard(cardData) {
+  const card = createCard(cardData);
+  cardSection.addItem(card);
+}
+//Create Card Function
+function createCard(cardData) {
+  const card = new Card(
+    cardData,
+    "#card-template",
+    handleImageClick,
+    handleDeleteButton,
+    handleLikeButton
+  );
+  return card.getCardElement();
+}
+//Click Card Function
+function handleImageClick(link, name) {
+  popupWithImage.open({ name, link });
+}
+
+function handleSubmit(
+  request,
+  popupInstance,
+  reset,
+  loadingText = "Saving..."
+) {
+  popupInstance.renderLoading(true, loadingText);
+  request()
+    .then(() => {
+      popupInstance.close();
+
+      if (reset) {
+        popupInstance.reset();
+      }
+    })
+    .catch(console.error)
+    .finally(() => {
+      popupInstance.renderLoading(false);
+    });
+}
+
+function handleEditProfileSubmit(inputValues) {
+  function makeRequest() {
+    return api.updateUserInfo(inputValues).then((userData) => {
+      userInfo.setUserInfo(userData);
+    });
+  }
+  handleSubmit(makeRequest, profileEditModal, true);
+}
+
+function handleAddCardSubmit(inputValues) {
+  function makeRequest() {
+    return api.createCard(inputValues).then((res) => {
+      renderCard(res);
+    });
+  }
+  handleSubmit(makeRequest, addCardPopup, true);
+}
+
+function handleDeleteButton(card) {
+  deleteImageModal.open();
+  return deleteImageModal.handleDelete(() => {
+    function makeRequest() {
+      return api.deleteCard(card.id).then(() => {
+        card.deleteCard();
+      });
+    }
+    handleSubmit(makeRequest, deleteImageModal, false, "Deleting...");
+  });
+}
+
+function handleLikeButton(card) {
+  if (card.isLiked) {
+    api
+      .deleteLike(card.id)
+      .then(() => {
+        card.handleIsLiked(false);
+      })
+      .catch((err) => {
+        console.log(`Unable to process request, ${err}`);
+      });
+  }
+  if (!card.isLiked) {
+    api
+      .likeCard(card.id)
+      .then(() => {
+        card.handleIsLiked(true);
+      })
+      .catch((err) => {
+        console.log(`Unable to process request, ${err}`);
+      });
+  }
+}
+
+function handleChangeProfilePicture(inputUrl) {
+  function makeRequest() {
+    return api.updateProfileImage(inputUrl).then((res) => {
+      userInfo.setProfileImage(res);
+    });
+  }
+  handleSubmit(makeRequest, profileImageModal, true);
+}
+
+//ProfileEditButtonEventListeners
+profileEditButton.addEventListener("click", () => {
+  const { name, about } = userInfo.getUserInfo();
+  profileTitleInput.value = name;
+  profileDescriptionInput.value = about;
+  profileEditModal.open();
+  formValidators["profile-form"].resetValidation();
+});
+//Add Card Button Event Listeners
+addNewCardButton.addEventListener("click", () => {
+  addCardPopup.open();
+  formValidators["add-card-form"].resetValidation();
+});
+
+profileImageEditButton.addEventListener("click", () => {
+  profileImageModal.open();
+  formValidators["profile-image-form"].resetValidation();
+});
+
+api
+  .getUserInfo()
+  .then((userData) => {
+    userInfo.setProfileImage(userData);
+    userInfo.setUserInfo({
+      name: userData.name,
+      about: userData.about,
+    });
+  })
+  .catch((err) => {
+    console.log(`Unable to process request, ${err}`);
+  });
+
+api
+  .getInitialCards()
+  .then((cards) => {
+    cardSection = new Section(
+      {
+        items: cards,
+        renderer: (cardData) => {
+          renderCard(cardData);
+        },
+      },
+      ".cards__list"
+    );
+    cardSection.renderItems(cards);
+  })
+  .catch((err) => {
+    console.log(`Unable to process request, ${err}`);
+  });
+
+// Form Validators
+
 const formValidators = {};
 
 const enableValidation = (config) => {
   const formList = Array.from(document.querySelectorAll(config.formSelector));
   formList.forEach((formElement) => {
     const validator = new FormValidator(config, formElement);
-    const formName = formElement.getAttribute("id");
+    const formName = formElement.getAttribute("name");
     formValidators[formName] = validator;
     validator.enableValidation();
   });
 };
 
 enableValidation(config);
-
-//const editFormValidator = new FormValidator(config, profileEditForm);
-//editFormValidator.enableValidation();
-//const cardAddFormValidator = new FormValidator(config, cardAddForm);
-//cardAddFormValidator.enableValidation();
-/* -------------------------------- UserInfo -------------------------------- */
-const profileInfo = new UserInfo(".profile__name", ".profile__description");
-/* ------------------------------ PopupWithForm ----------------------------- */
-const profileEditPopup = new PopupWithForm({
-  popupSelector: "#profile-edit-modal",
-  handleFormSubmit: handleProfileEditSubmit,
-});
-profileEditPopup.setEventListeners();
-const cardAddPopup = new PopupWithForm({
-  popupSelector: "#card-add-modal",
-  handleFormSubmit: handleCardAddSubmit,
-});
-cardAddPopup.setEventListeners();
-/* ----------------------------- PopupWithImage (Card)----------------------------- */
-const cardImagePopup = new PopupWithImage("#image-modal");
-cardImagePopup.setEventListeners();
-/* --------------------------------- Section -------------------------------- */
-const cardList = new Section(
-  {
-    items: initialCards,
-    renderer: renderCard,
-  },
-  ".cards__list"
-);
-cardList.renderItems();
-
-/* -------------------------------------------------------------------------- */
-/*                                  Functions                                 */
-/* -------------------------------------------------------------------------- */
-/* --------------------------------- Profile -------------------------------- */
-function fillProfileForm() {
-  const userData = profileInfo.getUserInfo();
-  profileTitleInput.value = userData.name;
-  profileDescriptionInput.value = userData.job;
-}
-/* ---------------------------------- Card ---------------------------------- */
-
-function handleImageClick(name, link) {
-  cardImagePopup.open(name, link);
-}
-
-function createCard(cardData) {
-  const card = new Card(cardData, "#card-template", handleImageClick);
-  const cardElement = card.generateCard();
-  return cardElement;
-}
-
-function renderCard(cardData) {
-  const cardElement = createCard(cardData);
-  cardList.addItem(cardElement);
-}
-/* -------------------------------------------------------------------------- */
-/*                               Event Handlers                               */
-/* -------------------------------------------------------------------------- */
-/* ------------------------------ Profile Edit ------------------------------ */
-function handleProfileEditSubmit(userData) {
-  profileInfo.setUserInfo(userData);
-  profileEditPopup.close();
-}
-function handleCardAddSubmit(formValues) {
-  const name = formValues.title;
-  const link = formValues.link;
-  renderCard({ name, link });
-  cardAddPopup.close();
-  cardAddForm.reset();
-  formValidators[cardAddForm.getAttribute("id")].disableButton();
-}
-/* -------------------------------------------------------------------------- */
-/*                               Event Listeners                              */
-/* -------------------------------------------------------------------------- */
-/* ------------------------------ Profile Edit ------------------------------ */
-profileEditButton.addEventListener("click", () => {
-  fillProfileForm();
-  //editFormValidator.resetValidation();
-  formValidators[profileEditForm.getAttribute("id")].resetValidation();
-  profileEditPopup.open();
-});
-/* ---------------------------------- Card ---------------------------------- */
-cardAddButton.addEventListener("click", () => {
-  //cardAddFormValidator.resetValidation();
-  cardAddPopup.open();
-});
